e0f620c2f5af412655241202dab8316a16e73d77
Jorge Sevilla <jsdelduca@gmail.com>
2023-05-08 08:09:50 -0400

    changes

--- controllers/session-controllers.js
+++ controllers/session-controllers.js
@@ -4,16 +4,16 @@ let redisClient = createClient()
 
 
 
-redisClient.connect().then( () => {
+redisClient.connect().then(() => {
     console.log('Client for handling sessions operations connected')
-}).catch( err => {console.log(err)})
+}).catch(err => { console.log(err) })
 
 
 let session;
 
 
 
-function getUsers(req, res){
+function getUsers(req, res) {
 
     redisClient.set('key', 'value').then(
         val => {
@@ -35,14 +35,14 @@ function getUsers(req, res){
 
 
 
-function createUser(req, res){
+function createUser(req, res) {
 
-  
-    let {name, email, password, repeatPassword, contact_phone } = req.body
-    
-    console.log(name, email, password, repeatPassword,contact_phone)
-    
-    
+
+    let { name, email, password, repeatPassword, contact_phone } = req.body
+
+    console.log(name, email, password, repeatPassword, contact_phone)
+
+
     let newUser = {
         name: name,
         email: email,
@@ -52,7 +52,9 @@ function createUser(req, res){
 
     let passwordsMatch = password === repeatPassword
 
-    if(passwordsMatch){
+    if (passwordsMatch) {
+
+        let profile_data = JSON.stringify(newUser)
 
         redisClient.set('userid', req.session.id).then(
             val => {
@@ -64,30 +66,99 @@ function createUser(req, res){
             }
         )
 
-        
 
-      
-        
-    
-    session = req.session
+        redisClient.set('profile', profile_data).then(
+            val => {
+                console.log('Profile saved')
+            }
+        ).catch(
+            err => {
+                console.log('Error saving id ', err)
+            }
+        )
 
-    req.session.profile = newUser
 
-    res.json({profile: req.session.profile, message: 'Usuario creado', created: true})
+
+
+        session = req.session
+
+        req.session.profile = newUser
+
+        res.json({ profile: req.session.profile, message: 'Usuario creado', created: true })
 
     }
     else {
-        res.json({message: 'Las contraseñas no coinciden', created: false})
+        res.json({ message: 'Las contraseñas no coinciden', created: false })
     }
-    
+}
+
+
+
+function addToCart(req, res) {
+
+
+
+    redisClient.get('profile').then(
+        profile => {
+            if(profile){
+                let profile_data = JSON.parse(profile)
+                let product = {title: 'motor', id: `ID${profile_data.cart.length}`, cantidad: 5}
+                profile_data.cart.push(product)
+                console.log(profile_data)
+                let parsedProfileData = JSON.stringify(profile_data)
+                redisClient.set('profile', parsedProfileData).then(
+                    () => {
+                        res.json({message: 'Product added to cart'})
+                    }
+                ).catch(
+                    err => {
+                        res.json({mesage: 'Error adding product to cart'})
+                    }
+                )
+            }
+            else {
+                res.json({message: 'There is no user logged'})
+            }
+        }
+    ).catch(
+        err => {
+            console.log('Redis error in cart function ', err)
+        }
+    )
+
 
 }
 
 
 
-function addToCart(){
-    console.log(req.body)
-    console.log(session.profile.cart)
+
+
+
+function loginUser() {
+
+}
+
+
+
+
+function removeFromCart(req, res){
+    let id = req.query.id
+    console.log(id)
+
+    redisClient.get('profile').then(
+        user => {
+            let parsedProfile = JSON.parse(user)
+            let index = parsedProfile.cart.findIndex( product => product.id === id)
+            parsedProfile.cart.splice(index, 1)
+            let userString = JSON.stringify(parsedProfile)
+            redisClient.set('profile', userString).then(
+                () => {
+                    res.json({message: 'Product removed from cart', parsedProfile})
+                }
+            )
+        
+        }
+    )
 
 }
 
@@ -95,29 +166,57 @@ function addToCart(){
 
 
 
+function updateQuantities(req, res){
+    let body = req.body
+    console.log('line 171 ', body)
 
-function loginUser(){
-    
-}    
+    if(body.ids.length > 1){
+        ids.forEach(
+            id => {
+                console.log(id)
+            }
+        )
+    }
+    else {
+
+        redisClient.get('profile').then(
+            profile => {
+                let parsedProfile = JSON.parse(profile)
+                console.log('185 ', parsedProfile)
+                let index = parsedProfile.cart.findIndex(
+                    product => product.id === body.ids
+
+                )
+                console.log(index)
+                console.log(parsedProfile.cart[index])
+            }
+        )
+        
+
+    }
+
+}
 
 
 
 
-function sessionChecker(req, res, next){
+function sessionChecker(req, res, next) {
     redisClient.get('userid').then(
         products => {
-            if(products){
+            if (products) {
                 console.log('user found')
+                res.json({ message: 'User found', logged: true })
 
             }
             else {
-                console.log('Products not found')
+                console.log('user not found')
+                res.json({ message: 'No user found', logged: false })
             }
         }
 
     ).catch(
         err => {
-            res.json({message: 'not found'})
+            res.json({ message: 'Error in redis ', err })
             console.log('not found')
         }
     )
@@ -134,4 +233,4 @@ function sessionChecker(req, res, next){
 }
 
 
-module.exports = { getUsers, sessionChecker, createUser }
+module.exports = { getUsers, sessionChecker, createUser, addToCart, removeFromCart, updateQuantities }
--- routes/session-routes.js
+++ routes/session-routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { getUsers, sessionChecker, createUser } = require('../controllers/session-controllers')
+const { getUsers, sessionChecker, createUser, addToCart, removeFromCart, updateQuantities } = require('../controllers/session-controllers')
 
 
 
@@ -20,10 +20,23 @@ sessionRouter.route('/get-session').get(getUsers)
 sessionRouter.route('/create-user').post(createUser)
 
 
+
+
 sessionRouter.route('/check-session').get(sessionChecker)
 
 
-sessionRouter.route('/add-to-cart').post()
+
+
+sessionRouter.route('/add-to-cart').post(addToCart)
+
+
+
+
+sessionRouter.route('/remove-from-cart').delete(removeFromCart)
+
+
+sessionRouter.route('/update-quantities').put(updateQuantities)
+
 
 
 module.exports = sessionRouter
